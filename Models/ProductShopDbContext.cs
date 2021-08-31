592d835d8bb995f8d033fc7c4030a17c34b398d1
RejinaldWild <68392463+RejinaldWild@users.noreply.github.com>
2021-08-31 16:38:46 +0300

    Added SQL

--- Models/ProductShopDbContext.cs
+++ Models/ProductShopDbContext.cs
@@ -11,7 +11,14 @@ namespace ProductShop.Models
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
             DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}ProductDb.db";
+
+
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=productshopdb;Trusted_Connection=True;");
+        }
+
         public DbSet<Product> Products { get; set; }
     }
 }
